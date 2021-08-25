2ddfa360c31b40636642de671be1e51d84de553b
Fabian <fafunas@gmail.com>
2021-08-25 20:10:33 -0300

    se repara el modelo

--- stock/models.py
+++ stock/models.py
@@ -5,11 +5,11 @@ from django.db.models.deletion import CASCADE
 
 # Create your models here.
 class product_group(models.Model):
-    codigo: models.CharField(max_length=3, verbose_name= 'Codigo')
-    Descripcion: models.CharField(max_length=30, verbose_name='Descripcion')
+    codigo= models.CharField(max_length=3, verbose_name= 'Codigo')
+    descripcion= models.CharField(max_length=30, verbose_name='Descripcion')
 
     def __str__(self):
-       return self.Descripcion
+        return self.descripcion
     class Meta:
         verbose_name = 'Grupo'
         verbose_name_plural = 'Grupos'
@@ -17,12 +17,11 @@ class product_group(models.Model):
         db_table = 'Grupos'
 
 class tipo(models.Model):
-    Grupo : models.ForeignKey(product_group, on_delete=CASCADE)
-    codigo: models.CharField(max_length=3, verbose_name= 'Codigo')
-    Descripcion: models.CharField(max_length=30, verbose_name='Descripcion')
+    codigo= models.CharField(max_length=3, verbose_name= 'Codigo')
+    descripcion= models.CharField(max_length=30, verbose_name='Descripcion')
 
     def __str__(self):
-       return self.Descripcion
+        return self.descripcion
     class Meta:
         verbose_name = 'Tipo'
         verbose_name_plural = 'Tipos'
@@ -32,15 +31,14 @@ class tipo(models.Model):
 class Producto(models.Model):
    grupo= models.ForeignKey(product_group, on_delete=models.CASCADE)
    tipo= models.ForeignKey(tipo, on_delete=CASCADE)
-   codigo = models.CharField(max_length=50, verbose_name='Codigo', null= True)
+   codigo = models.CharField(max_length=50, verbose_name='Codigo', null= True, blank=True)
    descripcion = models.CharField(max_length=50, verbose_name='Descripcion')
    date_created = models.DateField(auto_now=True)
    date_update = models.DateField(auto_now_add=True)
    
 
-
    def __str__(self):
-       return self.codigo
+       return self.descripcion
 
    class Meta:
        verbose_name = 'Producto'
