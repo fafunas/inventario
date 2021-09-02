96318b701e77d37a4fc8b69687f2b8efafe6a075
Fabian <fafunas@gmail.com>
2021-09-01 23:37:15 -0300

    Se crea listado de productos

--- stock/views.py
+++ stock/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import redirect, render
 from django.http import HttpResponse
-from django.template import RequestContext, loader
+from django.template import RequestContext, context, loader
 from stock.forms import CargarForm
 from stock.models import Producto, product_group, tipo
+from django.views.generic import ListView
 
 # Create your views here.
 
@@ -10,10 +11,11 @@ def index(request):
 
     return render (request, 'stock/index.html')
 
-   
+
+#Funcion para agregar producto a la DB   
 def cargar_producto(request):
-    if request.method == 'POST':
-        form = CargarForm(request.Post)
+    if request.POST == 'POST':
+        form = CargarForm(request.POST)
 
         if form.is_valid():
             grupo = form.cleaned_data['grupo']
@@ -21,11 +23,23 @@ def cargar_producto(request):
             descripcion = form.cleaned_data['descripcion']
             stock_minimo = form.cleaned_data['stock_minimo']
 
-            newdoc = Producto(grupo=grupo, tipo= tipo, descripcion= descripcion, stock_minimo= stock_minimo)
+            newdoc = Producto(grupo = grupo, tipo = tipo, descripcion = descripcion, stock_minimo = stock_minimo)
             newdoc.save()
             return redirect("index")
     else:
         form =CargarForm()
     return render(request, 'stock/alta_prod.html', {'form':form})
     
-    
+
+#Listado de productos
+
+class ListProductos(ListView):
+    model = Producto
+    template_name = 'stock/listview.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Listado de Productos'
+
+
+        return context
