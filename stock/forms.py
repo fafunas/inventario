b3fe822b2217cfae5a2e8b8af708d9d7fa7184a6
Fabian <fafunas@gmail.com>
2021-09-10 15:50:24 -0300

    Modificacon modelos

--- stock/forms.py
+++ stock/forms.py
@@ -2,12 +2,12 @@ from django.db.models.base import Model
 from django.forms import ModelForm
 from .models import Producto
 
-
+#Ingreso nueva producto
 class CargarForm(ModelForm):
 
     class Meta:
         model = Producto
-        fields = ['grupo','tipo','descripcion','stock_minimo']
+        fields = ['grupo','tipo','descripcion','stock_minimo','cco']
         error_messages = {
             'grupo': {
                 'required': ("Se debe agregar un grupo"),
@@ -17,10 +17,14 @@ class CargarForm(ModelForm):
             },
            'descripcion': {
                'required': ("Se debe agregar una descripcion del producto"),
-           },
+            },
            'stock_minimo': {
                'required': ("El stock minimo debe ser mayor a 0"),
-           }
+            }
+            
        }
     def __init__(self, *args, **kwargs):
       super(CargarForm, self).__init__(*args, **kwargs)
+
+
+#Ingreso producto al Stock
