5b7969b78c5af5afb7df5d0cb19fdf7b847fc101
Fabian <fafunas@gmail.com>
2021-09-17 14:05:40 -0300

    se agregan items al form

--- static/js/stock/form.js
+++ static/js/stock/form.js
@@ -1,8 +1,42 @@
 var produ = {
     items:{
         products:[]
-    }
-}
+    },
+    add: function(item){
+        this.items.products.push(item);
+        this.list();
+    }, 
+    list: function () {
+        $('#datain').DataTable({
+            responsive: true,
+            autoWidth: false,
+            destroy: true,
+            data: this.items.products,
+            columns: [
+                {"data": "descripcion"},
+                {"data": "cantidad"},
+                {"data": "date"},
+                {"data": "nro_remito"},
+                {"data": "nro_rq"},
+                {"data": "observacion"},
+            ],
+            columnDefs: [
+                {
+                    targets: [1,2,3,4,5],
+                    class: 'text-center',
+                    orderable: false,
+                    render: function (data, type, row) {
+                        return '<input type="text" name="cant" class="form-control form-control-sm" autocomplete="off" >';
+                    }
+                },
+             
+            ],
+            initComplete: function (settings, json) {
+
+            }
+        });
+    },  
+};
 
 $(function () {
     // Buscar Producto
